980a126686c1d606ebb4c43a058e7092d70ae925
HannaNikola <h.nikolaienko32@gmail.com>
2024-03-14 18:36:39 +0100

    update

--- controllers/avatarControllers.js
+++ controllers/avatarControllers.js
@@ -1,58 +1,44 @@
+
+
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
 import User from "../models/user.js";
 import Jimp from "jimp";
 
 export const uploadAvatar = async (req, res, next) => {
-    try {
-        await fs.rename(
-            req.file.path, path.join(process.cwd(), "public/avatars", req.file.filename));
-        const image = await Jimp.read(req.file.path);
-        await image.resize(250, 250);
-        await image.writeAsync(req.file.path);
+  try {
+    
+    const image = await Jimp.read(req.file.path);
 
+    await image.resize(250, 250);
 
-
-        const user = await User.findOneAndUpdate(
-        req.user.id,
-        { avatar: req.file.filename},
-        { new: true }
+    
+    const tempPath = path.join(
+      process.cwd(),
+      "public",
+      "avatars",
+      req.file.filename
     );
-    if (user === null) {
-        return res.status(404).send({massage: "User not found"})
+    await image.writeAsync(tempPath);
+
+  
+    await fs.rename(req.file.path, tempPath);
+
+    
+    const user = await User.findOneAndUpdate(
+      req.user.id,
+      { avatar: req.file.filename },
+      { new: true }
+    );
+
+   
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
     }
-        res.send(user);
-    } catch (error) {
-        next(error);
-    }
 
-}
-
-// export const uploadAvatar = async (req, res, next) => {
-//     try {
-        
-//        const resultUpload = await fs.rename(
-//          req.file.path,
-//          path.join(process.cwd(), "public/avatars", req.file.filename)
-//        );
-//         const image = await Jimp.read(resultUpload);
-//         await image.resize(250, 250);
-//         await image.writeAsync(resultUpload);
-
-
-
-//         const user = await User.findOneAndUpdate(
-//         req.user._id,
-//         { avatar: req.file.filename},
-//         { new: true }
-//     );
-//     if (user === null) {
-//         return res.status(404).send({massage: "User not found"})
-//     }
-//         res.send(user);
-//     } catch (error) {
-//         next(error);
-//     }
-
-// };
-
+    
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+};
