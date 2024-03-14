47f863069e13e52aef3023b626e06db2fccb3b5b
HannaNikola <h.nikolaienko32@gmail.com>
2024-03-14 14:37:28 +0100

    update

--- controllers/avatarControllers.js
+++ controllers/avatarControllers.js
@@ -3,46 +3,18 @@ import * as path from "node:path";
 import User from "../models/user.js";
 import Jimp from "jimp";
 
-// export const uploadAvatar = async (req, res, next) => {
-//     try {
-//         await fs.rename(
-//             req.file.path, path.join(process.cwd(), "public/avatars", req.file.filename));
-//         const image = await Jimp.read(avatar);
-//         await image.resize(250, 250);
-//         await image.writeAsync(avatar);
-
-
-
-//         const user = await User.findOneAndUpdate(
-//         req.user.id,
-//         { avatar: req.file.filename},
-//         { new: true }
-//     );
-//     if (user === null) {
-//         return res.status(404).send({massage: "User not found"})
-//     }
-//         res.send(user);
-//     } catch (error) {
-//         next(error);
-//     }
-
-// }
-
 export const uploadAvatar = async (req, res, next) => {
     try {
-        
-       const resultUpload = await fs.rename(
-         req.file.path,
-         path.join(process.cwd(), "public/avatars", req.file.filename)
-       );
-        const image = await Jimp.read(resultUpload);
+        await fs.rename(
+            req.file.path, path.join(process.cwd(), "public/avatars", req.file.filename));
+        const image = await Jimp.read(req.file.path);
         await image.resize(250, 250);
-        await image.writeAsync(resultUpload);
+        await image.writeAsync(req.file.path);
 
 
 
         const user = await User.findOneAndUpdate(
-        req.user._id,
+        req.user.id,
         { avatar: req.file.filename},
         { new: true }
     );
@@ -54,26 +26,33 @@ export const uploadAvatar = async (req, res, next) => {
         next(error);
     }
 
-};
+}
 
-// export const uploadAvatar = async (req, res) => {
-//   const { _id } = req.user;
-//   const { path: tempUpload, originalname } = req.file;
+// export const uploadAvatar = async (req, res, next) => {
+//     try {
+        
+//        const resultUpload = await fs.rename(
+//          req.file.path,
+//          path.join(process.cwd(), "public/avatars", req.file.filename)
+//        );
+//         const image = await Jimp.read(resultUpload);
+//         await image.resize(250, 250);
+//         await image.writeAsync(resultUpload);
 
-//   const filename = `${_id}_${originalname}`;
-//   const resultUpload = path.join(avatarsDir, filename);
 
 
+//         const user = await User.findOneAndUpdate(
+//         req.user._id,
+//         { avatar: req.file.filename},
+//         { new: true }
+//     );
+//     if (user === null) {
+//         return res.status(404).send({massage: "User not found"})
+//     }
+//         res.send(user);
+//     } catch (error) {
+//         next(error);
+//     }
 
-//   await fs.rename(tempUpload, resultUpload);
-
-//   const avatar = await Jimp.read(resultUpload);
-//   const resizeAvatar = await avatar.resize(250, 250);
-//   await resizeAvatar.write(resultUpload);
-
-//   const avatarURL = path.join("avatars", filename);
-//   await User.findByIdAndUpdate(_id, { avatarURL });
-
-//   res.json({ avatarURL });
 // };
 
